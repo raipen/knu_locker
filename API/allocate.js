f706d57c8651c97f6595da43d0eacc6525b8f47d
Jiwon <raipen@knu.ac.kr>
2022-02-16 12:32:03 +0900

    add API Folder

---
+++ API/allocate.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
+const properties = require('/web/properties.js');
+var mysql = require('mysql');
+
+module.exports ={
+  main:function(){
+    
+  }
+}
