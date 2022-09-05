21578fb1e1fa34c32a379a85514e80c0256fe330
Jiwon <raipen@knu.ac.kr>
2022-09-06 01:05:54 +0900

    add logger

--- src/api/locker.js
+++ src/api/locker.js
@@ -2,18 +2,21 @@ const express = require('express');
 const router = express.Router();
 const {errorCatcher,errorHandling} = require('./asyncErrorWraper');
 const LockerService = require('../services/LockerService');
+const logger = require('../log');
 
 router.get('/checkDues', errorCatcher(async (req, res) => {
   const userDTO = req.query;
+  console.log(`[/users/checkDues] ${userDTO.name} ${userDTO.number}`);
+  logger(req,"[/users/checkDues]",userDTO);
   const result = await LockerService.checkDues(userDTO);
-  console.log(`[/users/checkDues] ${userDTO.name}`);
   res.status(200).json(result);
 }));
 
 router.post('/fetchApply', errorCatcher(async (req, res) => {
   const userDTO = req.body;
+  console.log(`[/users/fetchApply] ${userDTO.name} ${userDTO.number}`);
+  logger(req,"[/users/fetchApply]",userDTO);
   const result = await LockerService.fetchApply(userDTO);
-  console.log(`[/users/fetchApply] ${userDTO.name}`);
   res.status(200).json(result);
 }));
 
