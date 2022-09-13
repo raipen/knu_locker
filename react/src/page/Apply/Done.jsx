0afcc947edc9f7b66a01a798822320bf6b04c509
Jiwon <raipen@knu.ac.kr>
2022-09-13 11:50:07 +0900

    add progress page

---
+++ react/src/page/Apply/Done.jsx
@@ -0,0 +1,7 @@
+export default function ({setStep,verifyToken}) {
+    return (
+        <h1>
+            신청 완료!
+        </h1>
+    )
+}
--- react/src/page/Apply/index.jsx
+++ react/src/page/Apply/index.jsx
@@ -1,48 +1,34 @@
 import { useRef, useState } from "react";
-import axios from 'axios';
-import Input from "../../component/Input";
-import InputPhone from "../../component/InputPhone";
-import SendButton from "../../component/SendButton";
+import Applicant from "./Applicant";
+import Done from "./Done";
+import SelectLocker from "./SelectLocker";
 import Steps from "./Steps";
 
 export default function Apply(){
     const [step,setStep] = useState(1);
-
-    const inputData = [
-        {id:"name",label:"이름",placeholder:"호반우"},
-        {id:"studentId",label:"학번",placeholder:"2022123456"},
-    ];
-    const phoneInputData = {id:"phone",label:"전화번호",placeholder:"'-'없이 숫자만 입력"};
-
-    const submitRef = useRef(null);
-    let phoneVerified = true;
-    //이름, 학번 정규식으로 체크하고, 전화번호 인증 했는지 확인하고 버튼 enable하기
-    const handleInputChange = (e) => {
-        const name = document.getElementById("name").value;
-        const studentId = document.getElementById("studentId").value;
-        const nameRegex = /^[가-힣| ]+$/;
-        const studentIdRegex = /^[0-9]{10}$/;
-        const sendButton = submitRef.current;
-        if(nameRegex.test(name) && studentIdRegex.test(studentId) && phoneVerified){
-            sendButton.disabled = false;
-        }else{
-            sendButton.disabled = true;
-        }
+    const [verifyToken,setVerifyToken] = useState("");
+    switch(step){
+        case 1:
+            return (
+                <article>
+                    <Steps step={step}/>
+                    <Applicant setStep={setStep} setVerifyToken={setVerifyToken}/>
+                </article>
+            );
+        case 2:
+            return (
+                <article>
+                    <Steps step={step} />
+                    <SelectLocker setStep={setStep} verifyToken={verifyToken}/>
+                </article>
+            );
+        default:
+            return (
+                <article>
+                    <Steps step={step}/>
+                    <Done/>
+                </article>
+            );
     }
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setStep(2);
-    }
-
-    return (
-        <article>
-            <Steps step={step}/>
-            <form onSubmit={handleSubmit}>
-                {inputData.map((info,i) => <Input info={info} key={i+1} event={handleInputChange}/>)}
-                <InputPhone info={phoneInputData} event={handleInputChange}/>
-                <SendButton reff={submitRef} disabled={true} text="다음"/>
-            </form>
-        </article>
-    )
 }
