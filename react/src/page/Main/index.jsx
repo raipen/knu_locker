968e3f0f7cae6925a605e987cdfabc596ddd9f9f
Jiwon <raipen@knu.ac.kr>
2022-09-29 13:23:16 +0900

    enable result menu

--- react/src/page/Main/index.jsx
+++ react/src/page/Main/index.jsx
@@ -8,9 +8,9 @@ export default function Main(){
             laptop_mac
             </span>
             <article className={styles.article}>
-                <Menu cName={styles.menu} href="/agree" icon="mouse" iconType="-outlined" text="사물함 추가 신청" date="개발 중"/>
+                <Menu cName={styles.menu +" "+ styles.disable} href="/" icon="mouse" iconType="-outlined" text="사물함 추가 신청" date="개발 중"/>
                 <Menu cName={styles.menu +" "+ styles.disable} href="/" icon="groups" text="개발중" date="개발중"/>
-                <Menu cName={styles.menu +" "+ styles.disable} href="/" icon="check_circle" text="사물함 신청 결과" date="9.28 ~"/>
+                <Menu cName={styles.menu} href="/result" icon="check_circle" text="사물함 신청 결과" date="9.28 ~"/>
             </article>
         </div>
     )
