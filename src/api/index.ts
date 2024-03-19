9fbafd4068d754223d2ce301246222a10fef734e
raipen <raipen@knu.ac.kr>
2024-03-20 00:38:37 +0900

    feat: delete test api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,13 @@
-const { Router,static } = require('express');
-const path = require('path');
+import express, { Router } from 'express';
+import path from 'path';
 
 const app = Router();
-app.use(static(path.join(__dirname, '../../react/dist')));
+app.use(express.static(path.join(__dirname, '../../react/dist')));
 app.use('/api/', require('./locker'));
 app.use('/API/', require('./locker'));
 app.use('/oauth/', require('./oauth'));
-app.use('/test/', require('./test'));
 app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname+ '../../../react/dist/index.html'));
 });
-module.exports = app;
+
+export default app;
