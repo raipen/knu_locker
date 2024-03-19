63d687bf969ea68ddcd4b5b52570dbd16941ef98
raipen <raipen@knu.ac.kr>
2024-03-19 20:16:23 +0900

    feat: allow lower case of api route

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const app = Router();
 app.use(static(path.join(__dirname, '../../react/build')));
+app.use('/api/', require('./locker'));
 app.use('/API/', require('./locker'));
 app.use('/test/', require('./test'));
 app.get('*',(req,res)=>{
