49828bc8a5c4744773bc54b358be6fb38b023168
raipen <raipen@knu.ac.kr>
2024-03-19 23:02:43 +0900

    feat: change front deploy folder

--- src/api/index.js
+++ src/api/index.js
@@ -2,12 +2,12 @@ const { Router,static } = require('express');
 const path = require('path');
 
 const app = Router();
-app.use(static(path.join(__dirname, '../../react/build')));
+app.use(static(path.join(__dirname, '../../react/dist')));
 app.use('/api/', require('./locker'));
 app.use('/API/', require('./locker'));
 app.use('/oauth/', require('./oauth'));
 app.use('/test/', require('./test'));
 app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname+ '../../../react/build/index.html'));
+    res.sendFile(path.join(__dirname+ '../../../react/dist/index.html'));
 });
 module.exports = app;
