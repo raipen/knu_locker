c2ef279de1cc2a35ee7cce020231361fa6ce9906
raipen <raipen@knu.ac.kr>
2024-03-21 02:18:52 +0900

    feat: add Input hook

--- react/src/component/Input.tsx
+++ react/src/component/Input.tsx
@@ -1,19 +1,38 @@
 import { InputStep } from "../utils/enum";
-import { useId } from "react";
+import { useId,useState } from "react";
 
-export default function Input({info}: {info:{
+export function useInput(regex: RegExp) {
+    const [value, setValue] = useState("");
+    const [step, setStep] = useState(InputStep.NONE);
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const v = e.target.value;
+        setValue(v);
+        if (regex.test(v)) return setStep(InputStep.COMPLETE);
+        return setStep(InputStep.NONE);
+    };
+    const onBlur = () => {
+        if (regex.test(value)) return setStep(InputStep.COMPLETE);
+        return setStep(InputStep.ERROR);
+    }
+    return {value, step, onChange, onBlur};
+}
+
+
+export function Input({info}: {info:{
     label: string,
     placeholder: string,
     type: "text"|"number",
     value: string,
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+    onBlur: () => void,
     step: InputStep,
 }}) {
     const id = useId();
     return (
         <div>
             <label htmlFor={id}>{info.label}</label>
-            <input type={info.type} name={id} placeholder={info.placeholder} value={info.value} onChange={info.onChange} />
+            <input type={info.type} name={id} placeholder={info.placeholder} value={info.value} onChange={info.onChange} onBlur={info.onBlur}/>
         </div>
     );
 }
