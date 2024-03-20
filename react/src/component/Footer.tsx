38d1f1f0f05eee5d6022bd698d167dbc5e01f012
raipen <raipendalk@gmail.com>
2024-03-20 16:02:16 +0900

    feat: convert some js to ts

---
+++ react/src/component/Footer.tsx
@@ -0,0 +1,12 @@
+export default function () {
+  return (
+    <footer>
+        <p>
+            Copyright 2022. KNU CSE student. 
+            <br/>
+            <a href="https://www.instagram.com/ji1_bahk/" rel="nofollow">@ji1_bahk</a>,
+            <a href="https://www.instagram.com/wlssyuu/" rel="nofollow"> designed by @wlssyuu</a>
+        </p>
+    </footer>
+  );
+}
--- react/src/component/Header.tsx
+++ react/src/component/Header.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-export default function Header({cName}) {
+export default function Header({cName}: {cName?: string}) {
   if(cName==="main"){
     document.body.className = "main";
   }else{
