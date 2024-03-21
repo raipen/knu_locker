4b09a6f2e691a481e17d6acf79c7eab6d9066db0
raipen <raipen@knu.ac.kr>
2024-03-22 02:13:44 +0900

    feat: convert allocate function js to ts

--- src/loaders/db.ts
+++ src/loaders/db.ts
@@ -1,7 +1,7 @@
 import { Express } from "express";
 import db from "../models";
 
-async function db_loader(app: Express) {
+async function db_loader(app?: Express) {
     await db.sync({force: false, alter: false});
 }
 
